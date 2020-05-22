0cf1bc02843b35c966a032a5c36ec96505cadb86
MoRahmanWork <mo.rahman.work@gmail.com>
2020-05-22 11:50:04 +0100

    commit

--- CandidateProjectUSS/FX_Carry_Strategy.py
+++ CandidateProjectUSS/FX_Carry_Strategy.py
@@ -50,7 +50,7 @@ class CurrencyMarkets:
         dateparse = lambda x: dateutil.parser.parse(x, dayfirst=True)
         rebaldf = pd.read_csv(self.path + '/data/RebalanceDates.csv',
                               parse_dates=['Date'], date_parser=dateparse, index_col='Date')
-        self.rebalancedates = rebaldf.to_dict(orient="index")
+        self.rebalancedates = rebaldf
 
     def getvix(self):
         # grab vix
@@ -58,19 +58,24 @@ class CurrencyMarkets:
         vixdf = pd.read_csv(self.path + '/data/VIX.csv',
                             parse_dates=['Date'], date_parser=dateparse, index_col='Date')
         vixdf = vixdf.rename(lambda x: 'VIX' if 'VIX' in x else x, axis=1)
-        self.vix = vixdf.to_dict(orient="index")
+        self.vix = vixdf
 
     def calcmarkets(self):
         # calculate forward rates
         for currency in self.currdict.keys():
             currdata = pd.merge(self.currdict[currency]['Spotdata'], self.currdict[currency]['Fwdpdata'], on='Date')
             currdata['Fwdrates'] = currdata['Spot'] + (self.currdict[currency]['fxMult'] * currdata['Fwdp'])
+
             if self.currdict[currency]['Quotation Style'] == 'American':
-                self.markets[currency]['Spot'] = currdata[['Spot']].to_dict(orient="index")
-                self.markets[currency]['Fwdrates'] = currdata[['Fwdrates']].to_dict(orient="index")
+                self.markets[currency]['Spot'] = currdata[['Spot']].rename(
+                    lambda x: currency if 'Spot' in x else x, axis=1)
+                self.markets[currency]['Fwdrates'] = currdata[['Fwdrates']].rename(
+                    lambda x: currency if 'Fwdrates' in x else x, axis=1)
             if self.currdict[currency]['Quotation Style'] == 'European':
-                self.markets[currency]['Spot'] = (1/currdata[['Spot']]).to_dict(orient="Index")
-                self.markets[currency]['Fwdrates'] = (1/currdata[['Fwdrates']]).to_dict(orient="index")
+                self.markets[currency]['Spot'] = 1/currdata[['Spot']].rename(
+                    lambda x: currency if 'Spot' in x else x, axis=1)
+                self.markets[currency]['Fwdrates'] = 1/currdata[['Fwdrates']].rename(
+                    lambda x: currency if 'Fwdrates' in x else x, axis=1)
 
     def runclass(self):
         self.getcurrencies()
@@ -80,10 +85,15 @@ class CurrencyMarkets:
         self.getvix()
         self.calcmarkets()
 
-
-
-
 dirpath = r'C:\Users\44794\PycharmProjects\Py4Fin\CandidateProjectUSS'
 markets = CurrencyMarkets(dirpath)
 markets.runclass()
+
+masterspot = pd.DataFrame()
+masterfwdrates = pd.DataFrame()
+for currency in markets.markets.keys():
+    masterspot = pd.concat([masterspot, markets.markets[currency]['Spot']], axis=1, join='outer').ffill()
+    masterfwdrates = pd.concat([masterfwdrates, markets.markets[currency]['Fwdrates']], axis=1, join='outer')
+
+
 
