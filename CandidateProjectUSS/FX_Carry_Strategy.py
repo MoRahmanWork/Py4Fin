bfcc9c17cd714a187648f3ed92b40f62c00af134
MoRahmanWork <mo.rahman.work@gmail.com>
2020-05-22 15:02:56 +0100

    commit

--- CandidateProjectUSS/FX_Carry_Strategy.py
+++ CandidateProjectUSS/FX_Carry_Strategy.py
@@ -6,6 +6,7 @@ import dateutil.parser
 
 class dict(dict):
     """create missing dictionary keys"""
+
     def __missing__(self, key):
         value = self[key] = type(self)()
         return value
@@ -13,12 +14,13 @@ class dict(dict):
 
 class CurrencyMarkets:
     """get market data"""
+
     def __init__(self, path=''):
         self.path = path
         self.currdict = dict()
         self.markets = dict()
-        self.rebalancedates = dict()
-        self.vix = dict()
+        self.rebalancedates = pd.DataFrame()
+        self.vix = pd.DataFrame()
 
     def getcurrencies(self):
         # grab currency, quotation styles, fxmult
@@ -29,34 +31,37 @@ class CurrencyMarkets:
     def getspot(self):
         # grab spots
         # identify format of every date in csvs & change to datetime
-        dateparse = lambda x: dateutil.parser.parse(x, dayfirst=True)
         for currency in self.currdict.keys():
             spotdf = pd.read_csv(self.path + '/data/' + currency + '_spot.csv',
-                                 parse_dates=['Date'], date_parser=dateparse, index_col='Date')
+                                 parse_dates=['Date'], date_parser=lambda x: dateutil.parser.parse(x, dayfirst=True),
+                                 index_col='Date')
             spotdf = spotdf.rename(lambda x: 'Spot' if currency in x else x, axis=1)
             self.currdict[currency]['Spotdata'] = spotdf
 
     def getforward(self):
         # grab forwards
-        dateparse = lambda x: dateutil.parser.parse(x, dayfirst=True)
+        # dateparse = lambda x: dateutil.parser.parse(x, dayfirst=True)
         for currency in self.currdict.keys():
             fwddf = pd.read_csv(self.path + '/data/' + currency + '_fwdp.csv',
-                                parse_dates=['Date'], date_parser=dateparse, index_col='Date')
+                                parse_dates=['Date'], date_parser=lambda x: dateutil.parser.parse(x, dayfirst=True),
+                                index_col='Date')
             fwddf = fwddf.rename(lambda x: 'Fwdp' if currency in x else x, axis=1)
             self.currdict[currency]['Fwdpdata'] = fwddf
 
     def getrebalancedates(self):
         # grab rebalance dates
-        dateparse = lambda x: dateutil.parser.parse(x, dayfirst=True)
+        # dateparse = lambda x: dateutil.parser.parse(x, dayfirst=True)
         rebaldf = pd.read_csv(self.path + '/data/RebalanceDates.csv',
-                              parse_dates=['Date'], date_parser=dateparse, index_col='Date')
+                              parse_dates=['Date'], date_parser=lambda x: dateutil.parser.parse(x, dayfirst=True),
+                              index_col='Date')
         self.rebalancedates = rebaldf
 
     def getvix(self):
         # grab vix
-        dateparse = lambda x: dateutil.parser.parse(x, dayfirst=True)
+        # dateparse = lambda x: dateutil.parser.parse(x, dayfirst=True)
         vixdf = pd.read_csv(self.path + '/data/VIX.csv',
-                            parse_dates=['Date'], date_parser=dateparse, index_col='Date')
+                            parse_dates=['Date'], date_parser=lambda x: dateutil.parser.parse(x, dayfirst=True),
+                            index_col='Date')
         vixdf = vixdf.rename(lambda x: 'VIX' if 'VIX' in x else x, axis=1)
         self.vix = vixdf
 
@@ -72,9 +77,9 @@ class CurrencyMarkets:
                 self.markets[currency]['Fwdrates'] = currdata[['Fwdrates']].rename(
                     lambda x: currency if 'Fwdrates' in x else x, axis=1)
             if self.currdict[currency]['Quotation Style'] == 'European':
-                self.markets[currency]['Spot'] = 1/currdata[['Spot']].rename(
+                self.markets[currency]['Spot'] = 1 / currdata[['Spot']].rename(
                     lambda x: currency if 'Spot' in x else x, axis=1)
-                self.markets[currency]['Fwdrates'] = 1/currdata[['Fwdrates']].rename(
+                self.markets[currency]['Fwdrates'] = 1 / currdata[['Fwdrates']].rename(
                     lambda x: currency if 'Fwdrates' in x else x, axis=1)
 
         masterspot = pd.DataFrame()
@@ -96,10 +101,70 @@ class CurrencyMarkets:
         self.getvix()
         self.calcmarkets()
 
+
+class Backtest(CurrencyMarkets):
+
+    def __init__(self, path='', notional=0, startdt='1/1/2000'):
+        super().__init__(path=path)
+        self.notional = notional
+        self.mins = pd.DataFrame()
+        self.maxs = pd.DataFrame()
+        self.startdt = startdt
+        self.enddt = dt.datetime.today()
+        self.allres = []
+
+    def runcurrencymarkets(self):
+        self.runclass()
+
+    def maxmin(self):
+        rebalrates = pd.concat([self.rebalancedates, self.markets['Fwdratesdf']], axis=1, join='inner')
+        self.mins = rebalrates.idxmin(axis=1, skipna=True)
+        self.maxs = rebalrates.idxmax(axis=1, skipna=True)
+
+    def calcweightscarry(self, date, method):
+        rebalancedrate = pd.concat([self.rebalancedates, self.markets['Fwdratesdf']], axis=1, join='inner')
+        rebalancedrate = rebalancedrate.loc[[date]]
+        longcur, longwei, shortcur, shortwei, longcar, shortcar = 0
+        if method == 1:
+            rankedrates = rebalancedrate.loc[[date]].rank(1, ascending=True, method='first')
+            longcur = rankedrates.loc[date].idxmax()
+            longwei = 1 / rankedrates.loc[date].max()
+            shortcur = rankedrates.loc[date].idxmin()
+            shortwei = -1 / rankedrates.loc[date].max()
+        elif method == 2:
+            test = ''
+        elif method == 3:
+            test = ''
+        elif method == 4:
+            test = ''
+
+        longcar = np.log(np.divide(self.markets['Spotdf'].loc[date, longcur],
+                                   self.markets['Fwdratesdf'].loc[date, longcur]))
+        shortcar = np.log(np.divide(self.markets['Spotdf'].loc[date, shortcur],
+                                   self.markets['Fwdratesdf'].loc[date, shortcur]))
+
+        return longcur, longwei, shortcur, shortwei, longcar, shortcar
+
+    def runbacktest(self):
+        datesrange = pd.bdate_range(start=self.startdt, end=self.enddt)
+
+        for dts in datesrange:
+            date = dts.date()
+            r = weightL*CarryL + weightS*CarryS
+            idxlvl = rebalidxlvl * (1 + r)
+            if date in self.rebalancedates.index:
+                emptylist = []
+                method = 1
+                longcurr, longweight, shortcurr, shortweight, longcarry, shortcarry = self.calcweightscarry(date, method)
+
+
+            print(dts)
+
+
 dirpath = r'C:\Users\44794\PycharmProjects\Py4Fin\CandidateProjectUSS'
-markets = CurrencyMarkets(dirpath)
-markets.runclass()
-
-rebalrates = pd.concat([markets.rebalancedates, markets.markets['Fwdratesdf']], axis=1, join='inner')
-
-
+notionals = 10000
+startdate = '1/1/2000'
+t = Backtest(dirpath, notionals, startdate)
+t.runclass()
+t.maxmin()
+t.runbacktest()
